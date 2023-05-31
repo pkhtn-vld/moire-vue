3b7064a17371b8642adadc070e13209888a5bb0c
pkhtn-vld <cemen331@mail.ru>
2023-05-31 18:21:12 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/moire-vue/',
+  publicPath: '/moire-vue/build/',
 });
